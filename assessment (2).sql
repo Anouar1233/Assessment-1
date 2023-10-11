739b11f066335a2f347eb195f90f2de60d897139
Anouar1233 <129745596+Anouar1233@users.noreply.github.com>
2023-10-11 10:44:21 +0200

    Add files via upload

---
+++ assessment (2).sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 11 okt 2023 om 10:43
+-- Serverversie: 10.4.28-MariaDB
+-- PHP-versie: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `assessment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `people`
+--
+
+CREATE TABLE `people` (
+  `name` varchar(50) NOT NULL,
+  `id` int(100) NOT NULL,
+  `function` varchar(100) NOT NULL,
+  `salary` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `people`
+--
+
+INSERT INTO `people` (`name`, `id`, `function`, `salary`) VALUES
+('Ewout ', 14, 'teacher', 1200),
+('Anouar', 16, 'intern', 400);
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `people`
+--
+ALTER TABLE `people`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `people`
+--
+ALTER TABLE `people`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
